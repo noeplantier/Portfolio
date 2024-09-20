8fa1cfed3afbda7afd4355af209872fa970807d5
Noé Plantier <plantiernoe50@gmail.com>
2024-09-20 19:03:42 +0200

    Portfolio initial style finished

--- portfolio/src/app/components/Projects.tsx
+++ portfolio/src/app/components/Projects.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image';
 import styles from '../styles/projects.module.scss';
 
 export default function Projects() {
   const projects = [
-    { title: 'O\'Party', description: 'Un site événementiel', image: '/images/oparty.png' },
-    { title: 'TECUM', description: 'Site de bijoux', image: '/images/tecum.png' }
+    { title: 'O\'Party', description: 'Website événementiel permettant aux utilisateurs de découvrir de nouveaux horizons', image: '/images/oparty.jpeg' },
+    { title: 'TECUM', description: 'Website officiel de la bijouterie TECUM', image: '/images/tecum.jpeg' },
+    { title: 'L\'immobilière de Julia', description: 'Website de l\'agence immobilière bretonne "L\'immobilière de Julia"', image: '/images/immo.jpeg' }
   ];
 
   return (
@@ -12,7 +14,13 @@ export default function Projects() {
       <div className={styles.projectList}>
         {projects.map((project, index) => (
           <div key={index} className={styles.projectCard}>
-            <img src={project.image} alt={project.title} />
+            <Image 
+              src={project.image} 
+              alt={project.title} 
+              width={300} 
+              height={200} 
+              className={styles.projectImage} 
+            />
             <h3>{project.title}</h3>
             <p>{project.description}</p>
           </div>
