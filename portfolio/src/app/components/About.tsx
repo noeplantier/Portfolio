47504f529ff97982a6a003051deb8bbf0c09d88c
Noé Plantier <plantiernoe50@gmail.com>
2024-09-20 22:14:35 +0200

    White/Blue style integration

--- portfolio/src/app/components/About.tsx
+++ portfolio/src/app/components/About.tsx
@@ -1,13 +1,27 @@
+"use client"
+
+import { useEffect, useState } from 'react';
 import styles from '../styles/about.module.scss';
 
 export default function About() {
+  const [displayedText, setDisplayedText] = useState('');
+  const fullText = 'Ennchanté ! Noé Plantier, développeur web full-stack avec une passion pour la création d\'expériences web et mobiles. Après une formation intense à l\'École O\'Clock. J\'ai acquis une solide expérience en React, Node.js, SCSS, TypeScript. Basé à Bordeaux, je suis motivé par l\'innovation et l\'élégance dans le code.';
+
+  useEffect(() => {
+    let index = 0;
+    const typingInterval = setInterval(() => {
+      setDisplayedText((prev) => prev + fullText[index]);
+      index++;
+      if (index === fullText.length) clearInterval(typingInterval);
+    }, 70); // Vitesse de frappe (1 ms entre chaque lettre)
+    return () => clearInterval(typingInterval);
+  }, []);
+
   return (
     <section id="about" className={styles.about}>
       <h2>À propos de moi</h2>
-      <p>
-        Je suis Noé Plantier, un développeur Fullstack Junior passionné par la création
-        de solutions web innovantes. Avec une expérience en React, Node.js, et SCSS,
-        j'ai travaillé sur divers projets allant de sites vitrine à des applications complexes.
+      <p className={styles.typingText} style={{ color: '#fffff'}}>
+        {displayedText}
       </p>
     </section>
   );
