ce6d32d7ba9f04aaa19a5e06241ad117f637ea2c
Noé Plantier <plantiernoe50@gmail.com>
2024-09-24 23:28:38 +0200

    logo's style fixes

--- portfolio/src/app/components/Hero.tsx
+++ portfolio/src/app/components/Hero.tsx
@@ -55,17 +55,66 @@ export default function Hero() {
         />
       </div>
       <div className={styles.background}>
-    
+      <img
+     src="https://cdn.iconscout.com/icon/free/png-256/free-html5-logo-icon-download-in-svg-png-gif-file-formats--html-wordmark-programming-langugae-language-pack-logos-icons-1175209.png"
+      alt="HTML5 Logo" 
+      className={styles.techLogo}
+     />
+    <img 
+    src="https://cdn.iconscout.com/icon/free/png-256/free-css3-logo-icon-download-in-svg-png-gif-file-formats--css-wordmark-programming-langugae-language-pack-logos-icons-1175238.png" 
+    alt="CSS3 Logo" 
+    className={styles.techLogo}
+    />
+        <img 
+    src="https://freesvg.org/img/1486641506.png" 
+    alt="JavaScript Logo" 
+    className={styles.techLogo}
+    />
+        <img 
+    src="https://icons.veryicon.com/png/o/business/vscode-program-item-icon/typescript.png"  
+    alt="TypeScript Logo" 
+    className={styles.techLogo}
+    />
+       <img
+          src="https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png"
+          alt="Node.js Logo"
+          className={styles.techLogo}
+        />
+          
         <img
           src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
           alt="React Logo"
           className={styles.techLogo}
         />
-        <img
-          src="https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png"
-          alt="Node.js Logo"
-          className={styles.techLogo}
-        />
+    
+  
+
+    <img 
+    src="https://iconape.com/wp-content/png_logo_vector/flutter-logo.png" 
+    alt="Flutter Logo" 
+    className={styles.techLogo}
+    />
+
+    <img 
+    src="https://seeklogo.com/images/P/php-logo-91EFDB12E1-seeklogo.com.png" 
+    alt="PHP logo" 
+    className={styles.techLogo}
+    />
+    <img 
+    src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png" 
+    alt="Python logo" 
+    className={styles.techLogo}
+    />
+    <img 
+    src="https://img.icons8.com/color/512/ruby-programming-language.png" 
+    alt="Ruby logo" 
+    className={styles.techLogo}
+    />
+    <img 
+    src="https://img.icons8.com/color/512/sass-avatar.png" 
+    alt="SASS logo" 
+    className={styles.techLogo}
+    />
       </div>
       <section className={styles.codeScreenshotsSection}>
         <div className={styles['code-screenshots']}>
