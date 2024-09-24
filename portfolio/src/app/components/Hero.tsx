39e100e7802f6326907d12787d7b256a75fb2ce3
Noé Plantier <plantiernoe50@gmail.com>
2024-09-24 22:39:54 +0200

    Pictures positions modifs

--- portfolio/src/app/components/Hero.tsx
+++ portfolio/src/app/components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   const [flipped, setFlipped] = useState({
     fullstack: false,
     backend: false,
-    frontend: false
+    frontend: false,
   });
 
   const fullText = "Full-Stack Web & Web Mobile Developer";
@@ -27,75 +27,107 @@ export default function Hero() {
   const handleMouseEnter = (imageType: string) => {
     setFlipped((prev) => ({
       ...prev,
-      [imageType]: true
+      [imageType]: true,
     }));
   };
 
   const handleMouseLeave = (imageType: string) => {
     setFlipped((prev) => ({
       ...prev,
-      [imageType]: false
+      [imageType]: false,
     }));
   };
 
   return (
     <section id="hero" className={styles.hero}>
+      {/* Titres H1 et H2 placés au-dessus de l'image */}
+      <div className={styles.headerText}>
+        <h1>Noé Plantier</h1>
+        <h2 className={styles.typingText}>{displayedText}</h2>
+      </div>
+
       <div className={styles.background}>
-        {/* Logos des technologies */}
-        <img src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt="React Logo" />
-        <img src="https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png" alt="Node.js Logo" />
-        {/* Autres logos */}
+        {/* Remettre les logos des technologies */}
+        <img
+          src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+          alt="React Logo"
+          className={styles.techLogo}
+        />
+        <img
+          src="https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png"
+          alt="Node.js Logo"
+          className={styles.techLogo}
+        />
+        {/* Ajouter d'autres logos si nécessaire */}
       </div>
-      <div className={styles.content}>
-        <div className="hero-background">
-          <img src="/images/CV-PIC.png" alt="Noé Plantier" className="hero-image" />
-        </div>
-        <section className="hero-section">
-          <h1>Noé Plantier</h1>
-          <h2 className={styles.typingText} style={{ color: '#ffffff' }}>
-            {displayedText}
-          </h2>
-          <a href="#projects" className={styles.button}>My Projects</a>
 
-          <div className={styles['code-screenshots']} id="code-screenshots">
-            <div
-              className={`${styles['image-container']} ${flipped.fullstack ? styles.flipped : ''}`}
-              onMouseEnter={() => handleMouseEnter('fullstack')}
-              onMouseLeave={() => handleMouseLeave('fullstack')}
-            >
-              <img
-                src={flipped.fullstack ? '/images/opartycode.png' : '/images/oparty.jpeg'}
-                alt="Noé Plantier Fullstack Project"
-                className={`${styles['fullstack-image']} ${flipped.fullstack ? styles.back : styles.front}`}
-              />
-            </div>
+      <div className={styles.heroImageContainer}>
+        <img
+          src="/images/CV-PIC.png"
+          alt="Noé Plantier"
+          className={styles.heroImage}
+        />
+      </div>
 
-            <div
-              className={`${styles['image-container']} ${flipped.backend ? styles.flipped : ''}`}
-              onMouseEnter={() => handleMouseEnter('backend')}
-              onMouseLeave={() => handleMouseLeave('backend')}
-            >
-              <img
-                src={flipped.backend ? '/images/immocode.png' : '/images/immo.jpeg'}
-                alt="Noé Plantier Backend Project"
-                className={`${styles['backend-image']} ${flipped.backend ? styles.back : styles.front}`}
-              />
-            </div>
-
-            <div
-              className={`${styles['image-container']} ${flipped.frontend ? styles.flipped : ''}`}
-              onMouseEnter={() => handleMouseEnter('frontend')}
-              onMouseLeave={() => handleMouseLeave('frontend')}
-            >
-              <img
-                src={flipped.frontend ? '/images/tecumcode.png' : '/images/tecum.jpeg'}
-                alt="Noé Plantier Frontend Project"
-                className={`${styles['frontend-image']} ${flipped.frontend ? styles.back : styles.front}`}
-              />
-            </div>
+      <section className={styles.codeScreenshotsSection}>
+        <div className={styles['code-screenshots']}>
+          <div
+            className={`${styles['image-container']} ${
+              flipped.fullstack ? styles.flipped : ''
+            }`}
+            onMouseEnter={() => handleMouseEnter('fullstack')}
+            onMouseLeave={() => handleMouseLeave('fullstack')}
+          >
+            <img
+              src={
+                flipped.fullstack
+                  ? '/images/opartycode.png'
+                  : '/images/oparty.jpeg'
+              }
+              alt="Noé Plantier Fullstack Project"
+              className={`${styles['fullstack-image']} ${
+                flipped.fullstack ? styles.back : styles.front
+              }`}
+            />
           </div>
-        </section>
-      </div>
+
+          <div
+            className={`${styles['image-container']} ${
+              flipped.backend ? styles.flipped : ''
+            }`}
+            onMouseEnter={() => handleMouseEnter('backend')}
+            onMouseLeave={() => handleMouseLeave('backend')}
+          >
+            <img
+              src={
+                flipped.backend ? '/images/immocode.png' : '/images/immo.jpeg'
+              }
+              alt="Noé Plantier Backend Project"
+              className={`${styles['backend-image']} ${
+                flipped.backend ? styles.back : styles.front
+              }`}
+            />
+          </div>
+
+          <div
+            className={`${styles['image-container']} ${
+              flipped.frontend ? styles.flipped : ''
+            }`}
+            onMouseEnter={() => handleMouseEnter('frontend')}
+            onMouseLeave={() => handleMouseLeave('frontend')}
+          >
+            <img
+              src={
+                flipped.frontend ? '/images/tecumcode.png' : '/images/tecum.jpeg'
+              }
+              alt="Noé Plantier Frontend Project"
+              className={`${styles['frontend-image']} ${
+                flipped.frontend ? styles.back : styles.front
+              }`}
+            />
+          </div>
+        </div>
+      </section>
     </section>
   );
 }
