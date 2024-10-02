9e4a96dbe64bccf05b75c6278d93bfb6b1d1a36c
Noé Plantier <plantiernoe50@gmail.com>
2024-10-02 12:51:27 +0200

    Protocols / next.config integration

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,13 +1,52 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['uxwing.com','cdn4.iconfinder.com','cdn.iconscout.com', 'freesvg.org', 'icons.veryicon.com', 'seeklogo.com', 'static-00.iconduck.com', 'upload.wikimedia.org','img.icons8.com'], 
+      remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'img.icons8.com',
+         },
+     
+        {
+            protocol: 'https',
+            hostname: 'uxwing.com',
+         },
+        {
+            protocol: 'https',
+            hostname: 'cdn4.iconfinder.com',
+         },
+        
+        {
+          protocol: 'https',
+          hostname: 'cdn.iconscout.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'freesvg.org',
+        },
+        {
+          protocol: 'https',
+          hostname: 'icons.veryicon.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'seeklogo.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'static-00.iconduck.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'upload.wikimedia.org',
+        },
+      ],
       deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
       imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-      formats: ['image/avif', 'image/webp'], // Formats d'image optimisés
+      formats: ['image/avif', 'image/webp'],
     },
-    reactStrictMode: true, // Option recommandée pour une meilleure détection des erreurs
-    swcMinify: true, // Minification pour de meilleures performances
+    reactStrictMode: true,
+    swcMinify: true,
   };
   
   export default nextConfig;
--- portfolio/src/app/components/Contact.tsx
+++ portfolio/src/app/components/Contact.tsx
@@ -2,25 +2,28 @@ import { useEffect, useState } from 'react';
 import styles from '../styles/contact.module.scss';
 
 export default function Contact() {
-  const [displayedText, setDisplayedText] = useState('');
-  const fullText = "If you\'d like to discuss a project or have any questions, feel free to reach out to me through the following methods :"
+  const [displayedText, setDisplayedText] = useState<string>(''); // Typage explicite de l'état
+  const fullText = "If you'd like to discuss a project or have any questions, feel free to reach out to me through the following methods:";
 
-    useEffect(() => {
-      let index = 0;
-      const typingInterval = setInterval(() => {
-        if (index < fullText.length) {
-          setDisplayedText((prev) => prev + fullText.charAt(index)); // Utilisation de charAt pour un accès sécurisé
-          index++;
-        } else {
-          clearInterval(typingInterval);
-        }
-      }, 70);
-      return () => clearInterval(typingInterval);
-    }, []);
+  useEffect(() => {
+    let index = 0;
+    const typingInterval = setInterval(() => {
+      if (index < fullText.length) {
+        setDisplayedText((prev) => prev + fullText.charAt(index)); // Utilisation de charAt pour un accès sécurisé
+        index++;
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, 70);
+
+    // Nettoyage de l'intervalle à la fin du cycle de vie du composant
+    return () => clearInterval(typingInterval);
+  }, []);
+
   return (
     <section id="contact" className={styles.contact}>
       <h1>Contact Me</h1>
-      <h2 className={styles.typingText} style={{ color: '#fffff'}}>
+      <h2 className={styles.typingText} style={{ color: '#fff' }}> {/* Correction de la couleur */}
         {displayedText}
       </h2>
       <form>
@@ -29,11 +32,11 @@ export default function Contact() {
           <input type="text" id="name" name="name" required />
         </div>
         <div className={styles.formGroup}>
-          <label htmlFor="name">Prénom</label>
+          <label htmlFor="surname">Prénom</label>
           <input type="text" id="surname" name="surname" required />
         </div>
         <div className={styles.formGroup}>
-          <label htmlFor="name">Téléphone</label>
+          <label htmlFor="phone">Téléphone</label>
           <input type="tel" id="phone" name="phone" required />
         </div>
         <div className={styles.formGroup}>
@@ -49,7 +52,3 @@ export default function Contact() {
     </section>
   );
 }
-function setDisplayedText(arg0: (prev: any) => any) {
-  throw new Error('Function not implemented.');
-}
-
